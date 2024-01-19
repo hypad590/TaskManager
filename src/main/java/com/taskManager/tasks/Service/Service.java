5249c8a20dca849302e2dcc320ebbd58e26a6071
hypad590 <aloxaryabov@gmail.com>
2024-01-19 18:11:15 +0300

    @Autowired

--- src/main/java/com/taskManager/tasks/Service/Service.java
+++ src/main/java/com/taskManager/tasks/Service/Service.java
@@ -2,10 +2,12 @@ package com.taskManager.tasks.Service;
 
 import com.taskManager.tasks.Repository.UsersTaskRepository;
 import com.taskManager.tasks.model.UsersTask;
+import org.springframework.beans.factory.annotation.Autowired;
 
 @org.springframework.stereotype.Service
 public class Service {
     private final UsersTaskRepository repos;
+    @Autowired
     public Service(UsersTaskRepository repos){
         this.repos = repos;
     }
